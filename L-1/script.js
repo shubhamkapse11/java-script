239a5c704d937670830542666a95bb41ac482673
shubhamkapse11 <shubhamkapse1110@gmail.com>
2026-04-24 14:44:14 +0530

    operators datatypes ans assignment

--- L-1/script.js
+++ L-1/script.js
@@ -1,1 +1,54 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+// console.log("This is a JavaScript file.");
+
+// let name = "shubh";
+// let name2 = 123 ;
+
+let fullName  = "animesh mern stack developer";
+
+console.log(fullName)
+
+
+
+// let name = "Rahul";  
+// let name2 = "Animesh";
+// console.log(name);
+// console.log(name2);
+
+
+// let age = 25; 
+// age = 30;
+// console.log(age);
+
+const pi = 3.14;
+//pi  = 2.15; // This will cause an error because pi is a constant
+const name = "Animesh"; // This is a constant variable
+
+var city = "New York";
+console.log(city);
+city = "Los Angeles";
+console.log(city);
+
+
+//    ---- string ----
+let message = "Hello, World!"; // " " or ' ' can be used for strings 
+const fullname = "Animesh";
+
+
+//  2. Number
+let age = 22;
+let price = 99.99;
+
+//  3. Boolean
+let isStudent = true; // true or false
+
+
+//  6. Combine Everything
+// 💻 Example
+let studentClassFive = "Amit";
+let ageOfStudent = 25;
+let isWorking = true;
+
+console.log(studentClassFive);
+console.log(ageOfStudent);
+console.log(isWorking);
---
+++ L-2/script.js
@@ -0,0 +1,44 @@
+
+// Example 1
+// let a = 2;
+// let b = 2;
+
+// let ans = a + b;
+// let ans = b - a;
+// let ans = a * b;
+// let ans = a / b;
+// let b = 3;
+// ans = a ** b;
+
+// let ans = a % b;
+// console.log(ans);
+
+
+
+//  Example
+//let x = 10;
+//x = x + 5;  // 15
+// Short Form
+//x += 5; // same as x = x + 5
+//  Other Examples
+// x -= 2;
+// x *= 3;
+// x /= 2;
+
+
+// == equal
+// === strict equal
+// != not equal
+// > greater than
+// < less than
+
+//  Example 1
+
+let a = 12;
+let b = 10;
+
+//  console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
