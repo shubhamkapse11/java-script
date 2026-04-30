a94a54f2878b97ba15303a4ce6cab7cce7ce99da
shubhamkapse11 <shubhamkapse1110@gmail.com>
2026-04-30 20:03:11 +0530

    Add new assignments and notes for Level 3; update HTML and JavaScript files

---
+++ L-2/script-revision-task.js
@@ -0,0 +1,60 @@
+// Calculator uses + - × ÷
+// Comparing marks → pass or fail
+
+
+//  List
+// + Addition
+// - Subtraction
+// * Multiplication
+// / Division
+// % Modulus (remainder)
+// 💻 Example 1
+
+let a = 11;
+let b = 5;
+
+let sum = a + b;
+console.log(sum)
+
+let val = a - b ;
+console.log(val)  // 5
+
+let multiply = a * b ;
+console.log(multiply)  // 50
+
+
+let div = a / b ;
+console.log(div) // 2
+
+let modulus = a % b ;
+
+
+let x = 10;
+//x = x + 5;  // 15
+// Short Form
+
+x += 5; // same as x = x + 5
+
+// 📌 Other Examples
+x -= 2;
+x *= 3;
+x /= 2;
+
+// let name = "rupesh";
+
+
+// Example 1
+// let a = 10;
+// let b = 5;
+
+console.log(a > b); // true
+//  Example 2 
+// ==   ===
+console.log(10 == "10");  // true
+console.log(10 === "10"); // false
+//  Explain Clearly
+// == → checks value only
+// === → checks value + type (IMPORTANT)
+
+
+
---
+++ L-3/script.js
@@ -0,0 +1,55 @@
+// revision of operators
+let a = 10;
+let b = "10";
+
+console.log(a==b) // true
+console.log(a === b ); //false
+
+
+let balance = 500;
+balance -= 100;
+
+console.log(balance);
+
+
+let a = 15;
+let b = 4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// ---- revision of previous topics end ----
+
+
+// Real-Life Examples
+// If it rains → take umbrella
+
+// If marks ≥ 35 → pass
+
+// If password correct → login
+
+ // if => kya ye sahi hai ya match ho raha hai \\
+ // else if =>
+ // else =>
+ 
+
+
+
+
+
+let login = false;
+let password = "abc123";
+
+if (password === "abc123") {
+    login = true;
+    console.log("Login successful");
+    console.log(login);
+}  else {
+ console.log("Login failed");
+ console.log(login);
+}
+
+
