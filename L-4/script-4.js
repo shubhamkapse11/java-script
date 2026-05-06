4d957fb0bbe1ea2daf7b76d4637bb0ec3130aa8a
shubhamkapse-vected <shubham.kapse@vectedtech.com>
2026-05-06 15:32:09 +0530

    Add notes and assignments for switch statements and loops; create index.html and script-4.js files

---
+++ L-4/script-4.js
@@ -0,0 +1,133 @@
+// let today = "monday";
+
+//   switch(today){
+//     case "saturday":
+//         console.log("Today is Saturday");
+//         break;
+//     case "sunday":
+//         console.log("Today is Sunday");
+//         break;
+//     case "tusday":
+//         console.log("Today is Monday");
+//         break;
+//     default:
+//         console.log("Today is not the weekend");
+//   }
+
+
+
+// let num = 10;
+
+// switch (num) {
+//   case 10:
+//     console.log("Number is 1");
+//    break;
+//   case 2:
+//     console.log("Number is 2");
+//     break;
+//   case 10:
+//     console.log("Number is 10 mil gya hai");
+//     break;
+//     default:
+//     console.log("Number is not 1, 2 or 10");
+// }
+
+
+// let month = "june";
+
+// switch (month){
+
+//     case "january":
+//         console.log("Month is January");
+//         break;
+//     case "february":
+//         console.log("Month is February");
+//         break;
+//     case "march":
+//         console.log("Month is March");
+//         break;
+//     default:
+//         console.log("Month is not January, February or March");
+// }
+
+
+
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+
+// for(let i=1; i<5; i++){
+//     console.log("Hello", i);
+// }
+
+// for(let a=5; a<10; a++){
+//     console.log("Number", a);
+// }
+
+
+
+
+// Example
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+
+// Syntax
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+// let day = 1;
+// do {
+//     console.log("Rupesh padoo");
+//     day++;
+// } while (day <= 5);
+
+
+// let name = "vihay";
+// let firstName = 'rupesh paddo ';
+
+// let dynamicString = `my name is 
+//                                      ${name} or main cover kar lunga syllabus ko `;
+
+
+//        console.log(name);
+//        console.log(firstName);
+//        console.log(dynamicString);
+
+
+let name = "vijay";
+let lastName = 'yadav';
+
+// let fullName = name + " " + lastName;
+// console.log(fullName);
+
+let nameInUpperCase = name.toUpperCase();
+
+console.log(nameInUpperCase);
+
+let lastNameInLowerCase = lastName.toLowerCase();
+
+console.log(lastNameInLowerCase);
+
+
+let nameLength = name.length;
+
+console.log(nameLength);
+
+
+let charAtIndex2 = name.charAt(5);
+
+console.log(charAtIndex2);
+console.log(name.charAt(2));
+
+
+
+
+console.log(name.indexOf("r"));
